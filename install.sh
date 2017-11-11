672426629f6fbbb19d28fb02ce7194a997b16cdc
Las <las@protonmail.ch>
2017-11-11 14:24:36 +0100

    Improve installation experience on linux
    install.sh now automatically finds the NS2 installation
    directory and uses the libsteam_api.so there through LD_RUN_PATH
    and symlinks.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -x
 
 
-which dub && LD_RUN_PATH='$ORIGIN' dub build -b release --compiler=ldc --force || test -e laspad || {
+which dub && LD_RUN_PATH='$ORIGIN/ns2:$ORIGIN' dub build -b release --compiler=ldc || test -e laspad || {
 	echo >&2 "Can not build executable and no executable presupplied!"
 	return 1
 }
@@ -13,3 +13,15 @@ mkdir -p "$INSTALL_DIR/lib/laspad"
 cp laspad          "$INSTALL_DIR/lib/laspad/"
 cp steam_appid.txt "$INSTALL_DIR/lib/laspad/"
 ln -sf "$INSTALL_DIR/lib/laspad/laspad" "$INSTALL_DIR/bin/laspad"
+ln -sf "$HOME/.local/share/Steam/steamapps/common/Natural Selection 2/x64" "$INSTALL_DIR/lib/laspad/ns2"
+
+sed -En 's:BaseInstallFolder_.*?"\s*"(.*?)":\1:p' < ~/.local/share/Steam/config/config.vdf |
+	while read dir
+	do
+		# I have no idea why the " is included
+		dir="${dir:1}/steamapps/common/Natural Selection 2"
+		test -e "$dir" && {
+			ln -sf "$dir/x64" "$INSTALL_DIR/lib/laspad/ns2"
+			exit 0
+		}
+	done
